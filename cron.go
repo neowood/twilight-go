73d3359ede48ff218e4a1b6c5cf19b582f22a77c
wuhao <dpwuhao@dfl.com.cn>
2019-12-03 13:29:07 +0800

    edit cron.go

---
+++ cron.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"time"
+
+	"github.com/labstack/gommon/log"
+	"github.com/robfig/cron/v3"
+)
+
+var c *cron.Cron
+
+func init() {
+	c = cron.New()
+	c.AddFunc("@every 10m", UpdateTwilightLine)
+	c.Start()
+}
+
+func UpdateTwilightLine() {
+	log.Info("schedule for updating")
+	sunPosition = GetSunPosByTime(time.Now().UTC())
+	GetTwilightLine(sunPosition)
+}
